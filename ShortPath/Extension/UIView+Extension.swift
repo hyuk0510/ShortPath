f7ece22c3ab0244821e51916c4c851b8aa90702b
Hyuk0510 <hyouk970510@gmail.com>
2025-12-09 23:01:28 +0900

    [ADD] Poi, text, Badge 추가

---
+++ ShortPath/Extension/UIView+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Extension.swift
+//  ShortPath
+//
+//  Created by 선상혁 on 12/7/25.
+//
+
+import UIKit
+
+extension UIView {
+    func asImage() -> UIImage {
+        UIGraphicsImageRenderer(bounds: self.bounds).image {
+            self.layer.render(in: $0.cgContext)
+        }
+    }
+}
+
--- ShortPath/MapViewController.swift
+++ ShortPath/MapViewController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 import KakaoMapsSDK
-import MapKit
 import SnapKit
-import Toast
 
 class MapViewController: UIViewController, MapControllerDelegate {
     
@@ -39,38 +37,66 @@ class MapViewController: UIViewController, MapControllerDelegate {
         return view
     }()
     
-    var mapContainer: KMViewContainer?
-    var mapController: KMController?
-    var _observerAdded: Bool = false
-    var _auth: Bool = false
-    var _appear: Bool = false
+    private let button = {
+        let view = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.image = UIImage(named: "noti")
+        view.configuration = config
+        return view
+    }()
+    
+    private let poiView = {
+        let view = UIView(frame: .init(x: 50, y: 50, width: 50, height: 50))
+        view.backgroundColor = .green
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "pin_green")
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        imageView.contentMode = .scaleAspectFit
+        
+        view.addSubview(imageView)
+        return view
+    }()
+    
+    private let poiBadgeView = {
+        let view = UIView(frame: .init(x: 15, y: 15, width: 15, height: 15))
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "noti")
+        imageView.frame = CGRect(x: 0, y: 0, width: 15, height: 15)
+        imageView.contentMode = .scaleAspectFit
+        
+        view.addSubview(imageView)
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapContainer = mapView
+        
+        guard mapContainer != nil else {
+            print("맵 생성 실패")
+            return
+        }
+        
+        mapController = KMController(viewContainer: mapContainer!)
+        mapController!.delegate = self
+                
         mapSetupUI()
-        addViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         addObservers()
         _appear = true
         
-        if _auth {
-            if mapController?.isEngineActive == false {
-                mapController?.activateEngine()
-            }
-        }
-    }
-   
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        if mapController == nil {
-            addViews()
+        if mapController?.isEnginePrepared == false {
+            mapController?.prepareEngine()
         }
         
-        mapController?.activateEngine()
+        if mapController?.isEngineActive == false {
+            mapController?.activateEngine()
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -118,44 +144,62 @@ class MapViewController: UIViewController, MapControllerDelegate {
     
     func authenticationSucceeded() {
         print("성공")
-        _auth = true
+        if _auth == false {
+            _auth = true
+        }
+        
+        if _appear && mapController?.isEngineActive == false {
+            mapController?.activateEngine()
+        }
     }
     
     private func mapSetupUI() {
         view.backgroundColor = .white
         
-        [mapView].forEach {
+        [mapView, button].forEach {
             self.view.addSubview($0)
         }
         
         mapView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        
+        button.snp.makeConstraints { make in
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.size.equalTo(50)
+        }
+        
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+    }
+    
+    @objc func buttonPressed() {
+        
     }
 
     func addViews() {
         let defaultPosition: MapPoint = MapPoint(longitude: 126.9137, latitude: 37.5491)
-        let mapViewInfo: MapviewInfo = MapviewInfo(viewName: "mapview", viewInfoName: "map", defaultPosition: defaultPosition, defaultLevel: 7)
-        
-        mapContainer = mapView
-        
-        guard mapContainer != nil else {
-            print("맵 생성 실패")
-            return
-        }
-        
-        mapController = KMController(viewContainer: mapContainer!)
-        mapController?.delegate = self
-        
-        mapController?.prepareEngine()
+        let mapViewInfo: MapviewInfo = MapviewInfo(viewName: "mapview", viewInfoName: "map", defaultPosition: defaultPosition, defaultLevel: 17)
         
         DispatchQueue.main.async {
             self.mapController?.addView(mapViewInfo)
         }
     }
     
+    func viewInit(viewName: String) {
+        print("OK")
+        
+        createLabelLayer()
+        createPoiStyle()
+        createPois()
+    }
+    
+    //addView 성공 이벤트 delegate. 추가적으로 수행할 작업을 진행한다.
     func addViewSucceeded(_ viewName: String, viewInfoName: String) {
         print("OK")
+        
+        kakaoMap.viewRect = mapContainer!.bounds    //뷰 add 도중에 resize 이벤트가 발생한 경우 이벤트를 받지 못했을 수 있음. 원하는 뷰 사이즈로 재조정.
+        viewInit(viewName: viewName)
     }
     
     func addViewFailed(_ viewName: String, viewInfoName: String) {
@@ -198,7 +242,7 @@ class MapViewController: UIViewController, MapControllerDelegate {
     }
     
     func showToast(_ view: UIView, message: String, duration: TimeInterval = 2.0) {
-        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width/2 - 150, y: view.frame.size.height-100, width: 300, height: 35))
+        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width / 2 - 150, y: view.frame.size.height - 100, width: 300, height: 35))
         toastLabel.backgroundColor = UIColor.black
         toastLabel.textColor = UIColor.white
         toastLabel.textAlignment = NSTextAlignment.center
@@ -218,5 +262,53 @@ class MapViewController: UIViewController, MapControllerDelegate {
             toastLabel.removeFromSuperview()
         })
     }
-}
+    
+    func createLabelLayer() {
+        let manager = kakaoMap.getLabelManager()
 
+        let layerOption = LabelLayerOptions(layerID: "PoiLayer", competitionType: .none, competitionUnit: .symbolFirst, orderType: .rank, zOrder: 10001)
+        let _ = manager.addLabelLayer(option: layerOption)
+    }
+    
+    func createPoiStyle() {
+        let manager = kakaoMap.getLabelManager()
+
+        let red = TextStyle(fontSize: 20, fontColor: UIColor.white, strokeThickness: 2, strokeColor: UIColor.red)
+        let blue = TextStyle(fontSize: 20, fontColor: UIColor.white, strokeThickness: 2, strokeColor: UIColor.blue)
+        
+        let textStyle1 = PoiTextStyle(textLineStyles: [
+            PoiTextLineStyle(textStyle: red)
+        ])
+        let textStyle2 = PoiTextStyle(textLineStyles: [
+            PoiTextLineStyle(textStyle: blue)
+        ])
+
+        let noti = PoiBadge(badgeID: "badge1", image: poiBadgeView.asImage(), offset: CGPoint(x: 0.9, y: 0.1), zOrder: 0)
+        let iconStyle = PoiIconStyle(symbol: poiView.asImage(), anchorPoint: CGPoint(x: 0.5, y: 0.0), badges: [noti])
+        let perLevelStyle = PerLevelPoiStyle(iconStyle: iconStyle, textStyle: textStyle1, level: 0)
+        let poiStyle = PoiStyle(styleID: "customStyle1", styles: [perLevelStyle])
+        manager.addPoiStyle(poiStyle)
+    }
+    
+    func createPois() {
+        let manager = kakaoMap.getLabelManager()
+
+        let layer = manager.getLabelLayer(layerID: "PoiLayer")
+        let poiOption = PoiOptions(styleID: "customStyle1")
+        poiOption.rank = 0
+        
+        poiOption.addText(PoiText(text: "testPoi", styleIndex: 0))
+        poiOption.clickable = true
+        
+        let poi1 = layer?.addPoi(option: poiOption, at: MapPoint(longitude: 126.9136, latitude: 37.5493))
+                
+        poi1?.show()
+    }
+    
+    var mapContainer: KMViewContainer?
+    var mapController: KMController?
+    lazy var kakaoMap: KakaoMap = mapController?.getView("mapview") as! KakaoMap
+    var _observerAdded: Bool = false
+    var _auth: Bool = false
+    var _appear: Bool = false
+}
