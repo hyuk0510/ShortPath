1704e5eca0fdba6d257010ba6a5bfe799528f318
Hyuk0510 <hyouk970510@gmail.com>
2026-05-06 17:10:41 +0900

    [FIX] 테스트앱 apikey에서 수정

--- ShortPath/Network/KakaoLocalManager.swift
+++ ShortPath/Network/KakaoLocalManager.swift
@@ -33,7 +33,7 @@ final class KakaoLocalManager {
         
         request.httpMethod = "GET"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("KakaoAK \(APIKey.kakaoAPI)", forHTTPHeaderField: "Authorization")
+        request.setValue("KakaoAK \(APIKey.releaseKakaoAPI)", forHTTPHeaderField: "Authorization")
         
         let (data, response) = try await URLSession.shared.data(for: request)
 
@@ -76,7 +76,7 @@ final class KakaoLocalManager {
         
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("KakaoAK \(APIKey.kakaoAPI)", forHTTPHeaderField: "Authorization")
+        request.setValue("KakaoAK \(APIKey.releaseKakaoAPI)", forHTTPHeaderField: "Authorization")
         request.httpBody = try JSONEncoder().encode(body)
         
         let (data, response) = try await URLSession.shared.data(for: request)
@@ -108,7 +108,7 @@ final class KakaoLocalManager {
         var request: URLRequest = URLRequest(url: url)
 
         request.httpMethod = "GET"
-        request.setValue("KakaoAK \(APIKey.kakaoAPI)", forHTTPHeaderField: "Authorization")
+        request.setValue("KakaoAK \(APIKey.releaseKakaoAPI)", forHTTPHeaderField: "Authorization")
 
         
         let (data, response) = try await URLSession.shared.data(for: request)
